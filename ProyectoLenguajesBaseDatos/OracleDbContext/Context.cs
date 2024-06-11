8fffad21e7416b7500d723c4bcba3f4ddce6f4b1
Josue <Josue@DESKTOP-NKF5GC8>
2024-06-11 17:53:08 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoLenguajesBaseDatos/OracleDbContext/Context.cs
@@ -0,0 +1,31 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace ProyectoLenguajesBaseDatos.OracleDbContext
+{
+    public class Context
+    {
+        private readonly OracleConnection _connection;
+
+        public Context(string connectionString) 
+        { 
+            _connection = new OracleConnection(connectionString);
+        }
+
+        public void Open() 
+        { 
+            if(_connection.State != ConnectionState.Open) 
+            { 
+                _connection.Open();
+            }
+        }
+
+        public void Close()
+        {
+            if(_connection.State != ConnectionState.Closed) 
+            { 
+                _connection.Close();
+            }
+        }
+    }
+}
---
+++ ProyectoLenguajesBaseDatos/Program.cs
@@ -0,0 +1,31 @@
+using ProyectoLenguajesBaseDatos.OracleDbContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<Context>(options => new Context(builder.Configuration.GetConnectionString("Connection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
