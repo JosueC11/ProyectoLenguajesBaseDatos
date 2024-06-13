594b68a3f9ee283b785f9d5d96b36ffea0661e55
Josue <Josue@DESKTOP-NKF5GC8>
2024-06-13 01:57:17 -0600

    Implementacion de Procedimiento Almacenado

--- ProyectoLenguajesBaseDatos/Models/Noticia.cs
+++ ProyectoLenguajesBaseDatos/Models/Noticia.cs
@@ -1,10 +1,12 @@
-﻿namespace ProyectoLenguajesBaseDatos.Models
+﻿using System.Data;
+
+namespace ProyectoLenguajesBaseDatos.Models
 {
     public class Noticia
     {
         public int Id { get; set; } = 0;
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public DateOnly PostedAt { get; set; }
+        public DateTime PostedAt { get; set; }
     }
 }
--- ProyectoLenguajesBaseDatos/Service/ServiceImplement/NoticiaImplement.cs
+++ ProyectoLenguajesBaseDatos/Service/ServiceImplement/NoticiaImplement.cs
@@ -15,18 +15,47 @@ namespace ProyectoLenguajesBaseDatos.Service.ServiceImplement
         }
         public List<Noticia> GetNoticias()
         {
+            var noticias = new List<Noticia>();
+
             try
             {
-                using (var connection = _context.GetConnection())
+                using (OracleConnection connection = _context.GetConnection())
                 {
-                    connection.Open();
-                }               
+                    using (OracleCommand cmd = new OracleCommand("ObtenerRegistros", connection))
+                    {
+                        var registros = new OracleParameter();
+                        registros.ParameterName = "registros";
+                        registros.OracleDbType = OracleDbType.RefCursor;
+                        registros.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(registros);
+
+                        connection.Open();
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        OracleDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            var noticia = new Noticia();
+
+                            noticia.Id = Convert.ToInt32(reader["ID_NOTICIA"]);
+                            noticia.Title = reader["TITLE"].ToString();
+                            noticia.Description = reader["DESCRIPTION"].ToString();
+                            noticia.PostedAt = Convert.ToDateTime(reader["POSTEDAT"]);
+
+                            noticias.Add(noticia);
+                        }
+
+                        connection.Close();
+                    }
+                }
+                return noticias;
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) 
+            { 
                 Console.WriteLine(ex.ToString());
+                return null;
             }
-            return null;
         }
         
         public void SetNoticias(Noticia noticias)
