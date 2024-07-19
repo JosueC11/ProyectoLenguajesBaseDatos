a74bb649f77c20565ed8d911856238e4a1a9f027
Josue <Josue@DESKTOP-NKF5GC8>
2024-07-18 20:54:28 -0600

    Agregando vista para las noticias y un layout

--- ProyectoLenguajesBaseDatos/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoLenguajesBaseDatos.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
